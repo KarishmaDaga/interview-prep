2258e5a11fb4b059bbbaab0dd07acdb478d8d45d
Karishma Daga <karishma.daga@queensu.ca>
2018-11-16 23:40:46 -0500

    completed containter with water

--- container-with-most-water.py
+++ container-with-most-water.py
@@ -1,1 +1,23 @@
-https://leetcode.com/problems/container-with-most-water/
+# https://leetcode.com/problems/container-with-most-water/
+
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        max_area = 0
+        left = 0
+        right = len(height) - 1
+        
+        while (left < right):
+            # calculate area
+            max_area = max(max_area, min(height[left], height[right]) * (right - left))
+            # increment the pointer
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -=1
+        return max_area
+            
+        
